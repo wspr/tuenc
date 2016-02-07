6e70e75a397eaca0fff7ef4244ee82a6524420a2
Will Robertson <will@wspr.io>
2016-02-07 23:24:17 +1030

    code dump
    it's working! in principle.

---
+++ umath2e/font-callback-mapping.lua
@@ -0,0 +1,36 @@
+
+require('math-map-gen.lua')
+require('mapping-math.lua')
+
+orig_define_font=luatexbase.remove_from_callback('define_font','luaotfload.define_font')
+
+function x_define_font(name,size,id)
+  local thisfont=orig_define_font(name,size,id)
+  this_mapping = math_maps[mathmapstr]
+  if (this_mapping and type(thisfont)=='table') then
+--    for k,v in pairs(thisfont) do
+--      print(k)
+--    end
+    print('\n*************\nDefine font callback mapping hook: ',mathmapstr)
+    for k,v in pairs(this_mapping) do
+	    thisfont.characters[k]=subst_glyph(thisfont,v)
+	  end
+  end
+  return thisfont
+end
+
+function subst_glyph(font,slot)
+  if not( font.characters[slot] ) then  return nil  end
+
+  local char={}
+  local entries = {"width","top_accent","italic","height","depth"}
+
+  -- serialise(font.characters[slot]) ERROR()
+  for i,v in pairs(entries) do
+    char[v] = font.characters[slot][v]
+  end
+  char.commands   = {{'char',slot}}
+  return char
+end
+
+luatexbase.add_to_callback('define_font',x_define_font,'my_define_font')
